6bca3f1bdf8f39a57bc8950adf23c1fc4b9bf900
papaparadox <tymurholovko@gmail.com>
2025-04-01 14:57:21 +0100

    Added functionality to handle votes on the page, user can vote and downvote - if request somehow fails to reach the database, user will get an error message and his vote won't be counted

--- src/components/main/CommentCard.jsx
+++ src/components/main/CommentCard.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
+import { downVoteCommentVotes, updateCommentVotes } from "../api";
+
 export default function CommentCard({ comment }) {
   const date = new Date(comment.created_at);
+  const [currentVotes, setCurrentVotes] = useState(0);
+  const [error, setError] = useState(null);
+
+  function handleVote() {
+    updateCommentVotes(comment.comment_id)
+      .then(() => {
+        setError(null);
+      })
+      .catch(() => {
+        setCurrentVotes((prev) => prev - 1);
+        setError("Failed to vote. Try again!");
+      });
+    setCurrentVotes(currentVotes + 1);
+  }
+  function handleDownVote() {
+    downVoteCommentVotes(comment.comment_id)
+      .then(() => {
+        setError(null);
+      })
+      .catch(() => {
+        setCurrentVotes((prev) => prev + 1);
+        setError("Failed to vote. Try again!");
+      });
+    setCurrentVotes(currentVotes - 1);
+  }
   return (
     <div className='comments'>
       <h4>{comment.author} writes:</h4>
       <p>{comment.body}</p>
       <p>Posted on: {date.toLocaleString()}</p>
-      <p>Votes: {comment.votes}</p>
+      <p>Votes: {comment.votes + currentVotes}</p>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleDownVote}>DownVote</button>
+      {error && <p id='vote-error'>{error}</p>}
     </div>
   );
 }
