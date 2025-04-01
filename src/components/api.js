4cbb80c1040a881f563e07311d6a2fcd833e13ba
papaparadox <tymurholovko@gmail.com>
2025-04-01 14:56:22 +0100

    Added functions to send a request to the API with intention of manipulating votes in comments

--- src/components/api.js
+++ src/components/api.js
@@ -21,3 +21,11 @@ export const getComments = (article_id) => {
     return data;
   });
 };
+
+export const updateCommentVotes = (comment_id) => {
+  return api.patch(`/comments/${comment_id}`, { inc_votes: 1 });
+};
+
+export const downVoteCommentVotes = (comment_id) => {
+  return api.patch(`/comments/${comment_id}`, { inc_votes: -1 });
+};
