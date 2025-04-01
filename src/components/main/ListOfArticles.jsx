561a59bbb65080a282cb5f1997b04e4a40b2ff5d
papaparadox <tymurholovko@gmail.com>
2025-04-01 11:25:57 +0100

    Updated element to have functions from api.js

--- src/components/main/ListOfArticles.jsx
+++ src/components/main/ListOfArticles.jsx
@@ -1,23 +1,21 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Article from "./Article";
+import ArticleCard from "./ArticleCard";
+import { getArticles } from "../api";
 
 export default function ListOfArticles() {
   const [articles, SetArticles] = useState([]);
 
   useEffect(() => {
-    const api = axios.create({
-      baseURL: "https://seeding-ncnews.onrender.com/api",
-    });
-    api.get("/articles").then(({ data }) => {
-      console.log(data.articles);
-      SetArticles(data.articles);
+    getArticles().then(({ articles }) => {
+      SetArticles(articles);
     });
   }, []);
+
   return (
-    <section class='articles-section'>
+    <section className='articles-section'>
       {articles.map((article) => {
-        return <Article key={article.title} article={article} />;
+        return <ArticleCard key={article.title} article={article} />;
       })}
     </section>
   );
