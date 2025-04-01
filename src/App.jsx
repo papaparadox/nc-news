c079217ab3e7e43401091402aa6ff45776d9f4bc
papaparadox <tymurholovko@gmail.com>
2025-04-01 11:21:55 +0100

    Added new route to ArticleById

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Header from "./components/header/Header";
 import { Routes, Route } from "react-router";
 import "./styles.css";
 import ListOfArticles from "./components/main/ListOfArticles";
+import ArticleById from "./components/main/ArticleById";
+
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route path='/' element={<ListOfArticles />} />
+        <Route path='/articles/:article_id' element={<ArticleById />} />
       </Routes>
     </>
   );
