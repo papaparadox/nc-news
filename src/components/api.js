2637dbe4ed103f5dab94c8af0afa96ea21e1b0ec
papaparadox <tymurholovko@gmail.com>
2025-04-01 11:20:19 +0100

    Added a baseUrl for all api requests, added function for fetching data from articles

---
+++ src/components/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://seeding-ncnews.onrender.com/api",
+});
+
+export const getArticles = () => {
+  return api.get("/articles").then(({ data }) => {
+    return data;
+  });
+};
+
+export const getArticleByID = (article_id) => {
+  return api.get(`/articles/${article_id}`).then(({ data }) => {
+    return data;
+  });
+};
