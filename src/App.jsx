cbc24e4c67952d5699d97aa9d442204593e5b9da
papaparadox <tymurholovko@gmail.com>
2025-04-02 16:05:33 +0100

    Added routes to topics, and then to articles with related topics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/header/Header";
 import { Routes, Route } from "react-router";
 import "./styles.css";
 import ListOfArticles from "./components/main/ListOfArticles";
+import ListOfTopics from "./components/main/ListOfTopics";
 import ArticleById from "./components/main/ArticleById";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<ListOfArticles />} />
         <Route path='/articles/:article_id' element={<ArticleById />} />
+        <Route path='/topics' element={<ListOfTopics />} />
+        <Route path='/:topic_name/articles' element={<ListOfArticles />} />
       </Routes>
     </>
   );
