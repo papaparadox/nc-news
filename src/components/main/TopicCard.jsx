23c7aea560dc3985991248aaa103f6ecf7d9883b
papaparadox <tymurholovko@gmail.com>
2025-07-23 10:38:16 +0100

    Added an id to the heading of topic to apply specific CSS to it

--- src/components/main/TopicCard.jsx
+++ src/components/main/TopicCard.jsx
@@ -4,7 +4,7 @@ export default function TopicCard({ topic }) {
   return (
     <div className='topic-card'>
       <Link to={`/${topic.slug}/articles`}>
-        <h1>{topic.slug}</h1>
+        <h1 id='topic-heading'>{topic.slug}</h1>
       </Link>
       <p>{topic.description}</p>
     </div>
